41e9a11217bb1e57dca8a2095770a2e6d2a865a7
jcoriell <josh.coriell@cyber.org>
2024-12-02 09:38:27 -0600

    first

---
+++ programs/05/files/template.py
@@ -0,0 +1,14 @@
+# Name:
+# Date:
+# Description:
+
+
+# define your classes here
+
+
+
+
+# main
+if __name__ == "__main__":
+    # test as you go by deleting the word pass and placing your own test code here
+    pass
---
+++ programs/06/files/template.py
@@ -0,0 +1,14 @@
+# Name:
+# Date:
+# Description:
+
+
+# Your code goes here
+
+
+
+
+# main
+if __name__ == "__main__":
+    # your testing code goes here
+    pass    # remove this line
