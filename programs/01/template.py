0bf864a314ddfb66a76618e08a9e6a9819d5cfc4
jcoriell <josh.coriell@cyber.org>
2024-12-08 10:34:11 -0600

    program 1 added

--- programs/01/template.py
+++ programs/01/template.py
@@ -1,3 +1,14 @@
 # Name:
 # Date:
 # Description: 
+
+
+
+# User defined functions (including a function called main)
+
+
+
+
+# Main portion of code for local testing
+if __name__ == "__main__":
+    pass   # remove pass
