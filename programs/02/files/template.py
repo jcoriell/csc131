b8044c58ecda0993e461e5ae19e111bef7793417
jcoriell <josh.coriell@cyber.org>
2024-12-14 12:27:19 -0600

    program 2 edits

---
+++ programs/02/files/template.py
@@ -0,0 +1,14 @@
+# Name:
+# Date:
+# Description:
+
+
+# user defined functions
+
+
+
+
+
+# main
+if __name__ == "__main__":
+    pass # replace this with your own tests as you build your functions
