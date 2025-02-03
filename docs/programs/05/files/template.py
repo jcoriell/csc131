d5e192bfecbc1037514052c3154041104d0a0d79
jcoriell <josh.coriell@cyber.org>
2025-02-03 08:51:47 -0600

    program 5

--- docs/programs/05/files/template.py
+++ docs/programs/05/files/template.py
@@ -3,12 +3,12 @@
 # Description:
 
 
-# define your classes here
+# Define your classes here
 
 
 
 
 # main
 if __name__ == "__main__":
-    # test as you go by deleting the word pass and placing your own test code here
-    pass
+    # place your tests here
+    pass  # remove this line
